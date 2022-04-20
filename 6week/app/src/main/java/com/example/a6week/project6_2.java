94f2ab91343ab48e91c63edc74e4504fc8fed471
Lee-yeonkyu <dldusrb46@gmail.com>
2022-04-20 20:11:56 +0900

    6주차 과제

---
+++ 6week/app/src/main/java/com/example/a6week/project6_2.java
@@ -0,0 +1,56 @@
+package com.example.a6week;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class project6_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project62);
+
+        EditText edtUrl = findViewById(R.id.edtUrl);
+        Button btnGo = findViewById(R.id.btnGo);
+        Button btnBack = findViewById(R.id.btnBack);
+        WebView web = findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(edtUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+
+    }
+
+    class CookWebViewClient extends WebViewClient{
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+    }
+}
